a39c281d5580a7892f17e193a0652b9fb260c20d
lcdf <9001053+fedriz@users.noreply.github.com>
2020-11-03 12:26:48 +0100

    Print all station on the map

--- code/api/meteoStations.js
+++ code/api/meteoStations.js
@@ -1,5 +1,29 @@
-import { BASE_PATH } from "./config";
+import { BASE_PATH, BASE_PATH_MOBILITY, BASE_PATH_TOURISM } from "./config";
 
-const requestMeteoStations = async () => {
-  const request = fetch(`${BASE_PATH}/Weather/Measuringpoint`);
+export const requestTourismMeasuringpoint = async () => {
+  try {
+    const request = await fetch(`${BASE_PATH_TOURISM}/Weather/Measuringpoint`);
+    if (request.status !== 200) {
+      throw new Error(request.statusText);
+    }
+    const response = await request.json();
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const requestMobilityMeteoStationSelectedData = async () => {
+  try {
+    const request = await fetch(
+      `${BASE_PATH_MOBILITY}/flat,node/MeteoStation/*?limit=-1&select=scoordinate,scode,sname,stype`
+    );
+    if (request.status !== 200) {
+      throw new Error(request.statusText);
+    }
+    const response = await request.json();
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 };
