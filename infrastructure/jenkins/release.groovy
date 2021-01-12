c1ff3938cacb31981e613a2fc5f60d193a6dee32
Peter Moser <p.moser@noi.bz.it>
2021-01-12 15:25:25 +0100

    infra: Use new Jenkins release script

--- infrastructure/jenkins/release.groovy
+++ infrastructure/jenkins/release.groovy
@@ -12,9 +12,13 @@ pipeline {
         string(name: 'VERSION', defaultValue: '1.0.0', description: 'Version (without a leading "v")', trim: true)
     }
     environment {
-        GIT_REPOSITORY = "git@github.com:noi-techpark/webcomp-meteo-generic.git"
-        GIT_BRANCH = "infra"
-        HERE_API_KEY = credentials("here-api-key")
+        // Web Components' default variables
+        WC_GIT_REMOTE = get_git_remote()
+        WC_GIT_BRANCH = get_git_branch()
+        WC_DIST_PATH = "dist"
+
+        // Your environmental variables
+        TILES_API_KEY = credentials("here-api-key")
     }
     stages {
         stage('Clean') {
@@ -29,7 +33,7 @@ pipeline {
             steps {
                 sh '''
                     rm -rf .env
-                    echo "HERE_API_KEY=${HERE_API_KEY}" >> .env
+                    echo "TILES_API_KEY=${TILES_API_KEY}" >> .env
                 '''
             }
         }
@@ -60,18 +64,29 @@ pipeline {
                         ssh-keyscan -H github.com >> ~/.ssh/known_hosts
                         git config --global user.email "info@opendatahub.bz.it"
                         git config --global user.name "Jenkins"
-                        git remote set-url origin ${GIT_REPOSITORY}
-                        git add dist/*
+                        git remote set-url ${WC_GIT_REMOTE} ${GIT_URL}
+                        git add ${WC_DIST_PATH}/*
                         git add -A
                         git commit --allow-empty -m "Version ${VERSION}"
                         git tag --delete v${VERSION} || true
-                        git push origin :v${VERSION} || true
+                        git push ${WC_GIT_REMOTE} :v${VERSION} || true
                         git tag -a v${VERSION} -m "Version ${VERSION}"
-                        git push origin HEAD:${GIT_BRANCH}
-                        git push origin v${VERSION}
+                        git push ${WC_GIT_REMOTE} HEAD:${WC_GIT_BRANCH}
+                        git push ${WC_GIT_REMOTE} v${VERSION}
                     """
                 }
             }
         }
     }
+}
+
+
+
+// Helper functions
+def get_git_remote() {
+    return env.GIT_BRANCH.split('/')[0]
+}
+
+def get_git_branch() {
+    return env.GIT_BRANCH.split('/')[1]
 }
